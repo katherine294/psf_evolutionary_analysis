5036577cb830b09f582f1409f8e41608eed6f4fb
katherine294 <75730954+katherine294@users.noreply.github.com>
2025-11-10 15:47:34 +0000

    Update master_pipeline.sh

--- Figure1a/master_pipeline.sh
+++ Figure1a/master_pipeline.sh
@@ -2,18 +2,18 @@ Run scripts 1 - 8.
 
 # itol was used to create the final tree figure.
 
-SBATCH 01.download_fastqc.sh
+sbatch 01.download_fastqc.sh
 
-SBATCH 02.trimmomatic.sh
+sbatch 02.trimmomatic.sh
 
-SBATCH 03.variant_calling.sh
+sbatch 03.variant_calling.sh
 
-SBATCH 04.core_alignment.sh
+sbatch 04.core_alignment.sh
 
-SBATCH 05.recombination_detection.sh
+sbatch 05.recombination_detection.sh
 
-SBATCH 06.MGE_masking.sh
+sbatch 06.MGE_masking.sh
 
-SBATCH 07.merge_sort_masking_bed.sh
+sbatch 07.merge_sort_masking_bed.sh
 
-SBATCH 08.Masked_core_SNP_tree.sh
+sbatch 08.Masked_core_SNP_tree.sh
